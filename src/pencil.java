69646bc5d76ddb40b2c292e62eac163946786c3f
georgebelt <george.belt@hotmail.com>
2014-10-03 17:41:00 -0400

    Added addition

--- src/pencil.java
+++ src/pencil.java
@@ -17,26 +17,57 @@ public class pencil {
     /**
      * @param args the command line arguments
      */
-    public class BigInteger {
-        int[] digit = new int[100];
-        int length = 0;
+    public static class BigInteger {
+        public int[] digit = new int[100];
+        public int length = 0;
+
+        public BigInteger() {
+            Arrays.fill(digit, 0);
+            length = 0;
+        }
+        
+        public BigInteger(String s) {
+            Arrays.fill(digit, 0);
+            length = s.length();
+            for (int i = 0; i < s.length(); i++) {
+                digit[length - i - 1] = s.charAt(i) - '0';
+            }
+        }
+        
         public void add(BigInteger other) {
-            int len = Math.max(length, other.length);
+            length = Math.max(length, other.length);
+            for (int i = 0; i < length; i++) {
+                digit[i] += other.digit[i];
+                if (digit[i] >= 10) {
+                    digit[i + 1] += digit[i] / 10;
+                    digit[i] = digit[i] % 10;
+                }
+            }
+            if (digit[length] > 0) {
+                length++;
+            }
         }
+        
         public void multiply(BigInteger other) {
-            BigInteger ret = new BigInteger();
+            
         }
-    }
-    public int multiplication(int a, int b) {
-        return a * b;
+        
+        public void print() {
+            for (int i = length - 1; i >= 0; i--) {
+                System.out.print(digit[i]);
+            }
+            System.out.println();
+        }
     }
     
     public static void main(String[] args) {
         int n = 1;
         Scanner stdin = new Scanner(System.in);
         while (stdin.hasNextLine()) {
-            int a = stdin.nextInt();
-            n *= a;
+            BigInteger a = new BigInteger(stdin.nextLine());
+            BigInteger b = new BigInteger(stdin.nextLine());
+            a.add(b);
+            a.print();
         }
         System.out.println(n);
     }
