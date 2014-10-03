778b6ee1f159211d24dd2a27d7af6ae01b37155a
georgebelt <george.belt@hotmail.com>
2014-10-03 15:14:02 -0400

    updated

--- src/pencil.java
+++ src/pencil.java
@@ -1,3 +1,7 @@
+
+import java.util.*;
+import java.lang.Math;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -13,8 +17,28 @@ public class pencil {
     /**
      * @param args the command line arguments
      */
+    public class BigInteger {
+        int[] digit = new int[100];
+        int length = 0;
+        public void add(BigInteger other) {
+            int len = max(length, other.length);
+        }
+        public void multiply(BigInteger other) {
+            BigInteger ret = new BigInteger();
+        }
+    }
+    public int multiplication(int a, int b) {
+        return a * b;
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int n = 1;
+        Scanner stdin = new Scanner(System.in);
+        while (stdin.hasNextLine()) {
+            int a = stdin.nextInt();
+            n *= a;
+        }
+        System.out.println(n);
     }
     
 }
